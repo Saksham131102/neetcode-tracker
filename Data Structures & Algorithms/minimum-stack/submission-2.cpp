8a6bec37df79434cfbeaf2e7bacd7a0da64a70d9
Saksham <113179290+Saksham131102@users.noreply.github.com>
2026-04-03 22:21:49 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.cpp
@@ -0,0 +1,28 @@
+class MinStack {
+    stack<int> st;
+    stack<int> minSt;
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        st.push(val);
+        if(!minSt.empty())
+            val = min(val, minSt.top());
+        minSt.push(val);
+    }
+    
+    void pop() {
+        st.pop();
+        minSt.pop();
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return minSt.top();
+    }
+};
