0ef82565419ba1c9be6265a5a597cc78713d69d4
Saksham <113179290+Saksham131102@users.noreply.github.com>
2026-09-16 02:13:27 +0530

    Add: subtree-of-a-binary-tree - submission-1

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-1.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+private:
+    string serialize(TreeNode* root) {
+        string res;
+        serialize(root, res);
+        return res;
+    }
+    void serialize(TreeNode* root, string &res) {
+        if(root == nullptr) {
+            res += "$#";
+            return;
+        }
+
+        res += "$";
+        res += to_string(root->val);
+        serialize(root->left, res);
+        serialize(root->right, res);
+    }
+    vector<int> z_function(string s) {
+        vector<int> z(s.length());
+        int l = 0, r = 0, n = s.length();
+        for(int i=1;i<n;i++) {
+            if(i <= r) {
+                z[i] = min(r - i + 1, z[i-l]);
+            }
+            while(i + z[i] < n && s[z[i]] == s[i+z[i]]) {
+                z[i]++;
+            }
+            if(i+z[i]-1 > r) {
+                l = i;
+                r = i + z[i] - 1;
+            }
+        }
+        return z;
+    }
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        string S_root = serialize(root);
+        string S_sub = serialize(subRoot);
+        string combined = S_sub + "|" + S_root;
+
+        vector<int> z_values = z_function(combined);
+        int sub_len = S_sub.length();
+
+        for(int i=sub_len+1;i<combined.length();i++) {
+            if(z_values[i] == sub_len)
+                return true;
+        }
+        return false;
+    }
+};
